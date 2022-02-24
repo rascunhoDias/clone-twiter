201b16e3a20a3df2733f25c2d5439527502386c2
rascunhoDias <joao.bugad00@gmail.com>
2022-02-24 20:06:12 -0300

    estrutura base do projeto

---
+++ Core/DataBase.php
@@ -0,0 +1,18 @@
+
+
+
+m     v     c 
+
+model -> modelo do banco de dados
+    user -> tabela do banco de dados
+        id -> id do usuario
+        pass -> senha do usuario
+        nome -> nome do usuario
+
+view
+    html -> html
+controller
+    ligação -> model -> view
+
+ 
+v -> c -> m -> c -> v
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ .  "/App/Controllers/HomeController.php";
+
+(new HomeController())->index();
